f8c0a5231037b552332e4e7f10a3d81ae4dd314d
asnisapps <asnisapps@gmail.com>
2020-06-06 16:30:30 -0300

    Layout tela total

---
+++ ComprasUSA/TotalViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TotalViewController.swift
+//  ComprasUSA
+//
+//  Created by Fabio Asnis Campos da Silva on 06/06/20.
+//  Copyright © 2020 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class TotalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
